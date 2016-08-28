8c9cf8e11773b4aef5989faaad6d8e5c7eccaaf3
Chethan <Chethan.palaksha@tigerspike.com>
2016-08-28 19:04:16 +0800

    workspace

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 export DEVELOPER_DIR=/Applications/Xcode-beta.app/Contents/Developer/
-xcodebuild -project TestSwiftProject.xcodeproj -scheme TestSwiftProject -archivePath builds/TestSwiftProject.xcarchive archive
+xcodebuild -workspace TestSwiftProject.xcworkspace -scheme TestSwiftProject -archivePath builds/TestSwiftProject.xcarchive archive
 
 xcodebuild -exportArchive -exportOptionsPlist exportPlist.plist -archivePath builds/TestSwiftProject.xcarchive -exportPath builds
